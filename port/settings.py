a1061584b62843b920ef6a2ec1d6af83520843ef
Aditi Kalan <84493674+aditikalan@users.noreply.github.com>
2022-02-12 22:18:02 +0530

    Update settings.py

--- port/settings.py
+++ port/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#feo%ir_-uo348jsmkuub%4o)tb!yw#%ku&f16qu0k_&mch#l!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1','aditisystem9.herokuapp.com']
 
